88e5f3ef62472b11d3d2ff5bffb54b22cc43a4ab
rickyseezy <binisoma@gmail.com>
2026-07-20 11:11:40 +0400

    test: expect PF-002 migration head in stack conformance

--- apps/launcher/internal/secretprojector/pristine_stack_runtime_test.go
+++ apps/launcher/internal/secretprojector/pristine_stack_runtime_test.go
@@ -129,7 +129,7 @@ func TestPF001PristineStackStartsNeo4jBeforeCommunityMigrations(t *testing.T) {
 	)
 	verify.Env = projectionTestEnvironment()
 	head, err := verify.Output()
-	if err != nil || string(head) != "0001_pf001_core\n" {
+	if err != nil || string(head) != "0002_pf002_projection_rebuild\n" {
 		t.Fatalf("pristine relational migration head = %q/%v", head, safePristineError(err))
 	}
 }
